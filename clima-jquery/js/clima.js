74f08dbf59bea6367276d18225625c4c9e10b920
David Eraso <gallo9.5@hotmail.com>
2016-07-21 21:30:07 -0500

    ejemplo de clima ajax de yahoo con jquery

---
+++ clima-jquery/js/clima.js
@@ -0,0 +1,14 @@
+var prevId;
+
+function loadWeather(id, city) {
+    if (prevId != null) {
+
+        $("#" + prevId).toggleClass("selected");
+
+    }
+    $("#" + id).toggleClass("selected");
+    prevId = id;
+
+    var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+city+"%2C%20co%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+
+}
