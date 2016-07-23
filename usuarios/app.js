4f899fa6a1e0d7a26d67e486d3f2734e535181d0
David Eraso <gallo9.5@hotmail.com>
2016-07-23 11:22:30 -0500

    proyecto gestion de usuarios con express y mongodb

--- usuarios/app.js
+++ usuarios/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongo = require('mongodb').MongoClient;
 
 //var routes = require('./routes/index');
 //var users = require('./routes/users');
@@ -11,6 +12,22 @@ var bodyParser = require('body-parser');
 var usuarios = require("./routes/usuarios");
 
 var app = express();
+
+var mDB;
+mongo.connect("mongodb://localhost:27017/usuariosDB", function (err, db) {
+  if (err) {
+    console.log("Error al conectarse a mongo");
+  } else {
+    console.log("Conexión Exitosa");
+    mDB = db;
+  }
+});
+
+app.use(function(req,res, next){
+  //conexión a la base de datos
+  req.db = mDB;
+  next();
+});
 
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
@@ -27,10 +44,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 //app.use('/', routes);
 //app.use('/users', users);
 
-app.use("/usuarios",usuarios);
+app.use("/usuarios", usuarios);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -41,7 +58,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -52,7 +69,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
---
+++ usuarios/routes/api.js
@@ -0,0 +1,62 @@
+var express = require("express");
+var router = express.Router();
+var mId = require("mongodb").ObjectId;
+
+//middleware que se ejecuta cada que llega el parametro
+//coleccion en la url
+router.param("collection", function (req, res, next, c) {
+    req.c = req.db.collection(c);
+    next();
+});
+
+//agregar recurso
+router.post("/:collection", function (req, res, next) {
+    var obj = req.body; //obtenemos el objeto a insertar
+    //insertamos el archivo en mongoDB, w:1 requerimos confirmación
+    req.c.insert(obj, { w: 1 }, function (err, result) {
+        if (err) {
+            res.send({ success: false });
+        } else {
+            //exito, y retornamos la id del recurso
+            res.send({ success: true, id: result.insertedIds[0] });
+        }
+    });
+});
+
+//eliminar recurso
+router.delete("/:collection:id", function (req, res, next) {
+    var id = new mId(req.params.id);
+    req.c.delete({ _id: id }, function (err, result) {
+        if (err) {
+            res.send({ success: false });
+        } else {
+            res.send({ success: true });
+        }
+    });
+})
+
+//obtener todos los usuarios
+router.get("/:collection", function (req, res, next) {
+    req.c.find().toArray(function (err, results) {
+        if (err) {
+            res.send([]);
+        } else {
+            res.send(results);
+        }
+    });
+});
+
+//obtener un solo usuario
+router.get("/:collection/:id", function (req, res, next) {
+    var id = new mId(req.params.id);
+    req.c.findOne({ _id: id }, function (err, result) {
+        if (err || result == null) {
+            res.status(404).send({ msg: "No encontrado" });
+
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+module.exports = router;
