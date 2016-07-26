bffa2463e1da18f6c4fedaabfbfb69766bca9588
David Eraso <gallo9.5@hotmail.com>
2016-07-26 17:50:36 -0500

    fin proyecto de prueba webpack

--- usuarios-client/src/app.js
+++ usuarios-client/src/app.js
@@ -1,3 +1,6 @@
 require("./js/logic1");
 require("./js/logic2");
 require("./js/logic3");
+
+require("./styles/main.css");
+require("./styles/main2.css");
--- usuarios-client/webpack.config.js
+++ usuarios-client/webpack.config.js
@@ -1,5 +1,6 @@
 var webpack = require("webpack");
 var htmlWebpackPlugin = require("html-webpack-plugin");
+var extractWebpackPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     entry: "./src/app",
@@ -7,10 +8,16 @@ module.exports = {
         filename: "build.js",
         path: "build"
     },
+    module:{
+        loaders:[
+            {test:/\.css$/,loader:extractWebpackPlugin.extract("style-loader","css-loader")}
+        ]
+    },
     plugins:[
         new webpack.optimize.UglifyJsPlugin(),
         new htmlWebpackPlugin({
             template: "index.html"
-        })
+        }),
+        new extractWebpackPlugin("style.css")
     ]
 };
